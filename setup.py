b6cf72a16bfdb209a2ef6913ac6a8cc48cf59d52
Ross McFarland <rwmcfa1@gmail.com>
2011-08-01 14:11:36 -0700

    quick pass at a setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from os.path import join, dirname
+
+try:
+    long_description = open(join(dirname(__file__), 'README.rst')).read()
+except Exception:
+    long_description = None
+
+setup(
+    name='asynchttp',
+    version='0.0.1',
+    description='A simple httplib2 based asynchronous HTTP library for python',
+    author='Ross McFarland',
+    author_email='ross@gmail.com',
+    url='http://github.com/ross/python-asynchttp',
+
+    long_description=long_description,
+    classifiers=[
+        # TODO: update
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+
+    packages=['asynchttp'],
+    provides=['asynchttp'],
+    requires=['httplib2(>=0.5.0)'],
+    install_requires=['httplib2(>=0.5.0)'],
+)
